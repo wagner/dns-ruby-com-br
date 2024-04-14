4fa4fc695c0182164246f1526a5caa4e5d92be98
Wagner Narde <5348+wagner@users.noreply.github.com>
2024-04-14 18:15:28 -0300

    Adiciona TXT e A do apex nos locals

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Repositório de registros DNS do domínio `ruby.com.br` gerenciados via Terraform. Aberto a qualquer finalidade da comunidade brasileira.
 
-Para incluir um novo registro ou modificar um existente, crie um pull request modificando o arquivo [records.tf](https://github.com/wagner/dns-ruby-com-br/blob/main/records.tf). Consulte a [documentação](https://registry.terraform.io/providers/hashicorp/aws/5.44.0/docs/resources/route53_record) do Terraform para mais informações.
+Para incluir um novo registro ou modificar um existente, crie um pull request modificando o arquivo [locals.tf](https://github.com/wagner/dns-ruby-com-br/blob/main/locals.tf). Consulte a [documentação](https://registry.terraform.io/providers/hashicorp/aws/5.44.0/docs/resources/route53_record) do Terraform para mais informações.
 
 O DNS está hospedado no Route53 da AWS em uma conta gerenciada e paga por [@wagner](https://github.com/wagner).
--- locals.tf
+++ locals.tf
@@ -1,47 +1,70 @@
 locals {
   records = {
+    # ruby.com.br -> https://github.com/rubysummitbrasil/rubysummitbrasil.github.io
+    # TODO: Mover para summit2021.ruby.com.br e usar o apex para outra coisa
+    apex_a = {
+      type = "A"
+      name = "ruby.com.br"
+      value = [
+        "185.199.108.153",
+        "185.199.109.153",
+        "185.199.110.153",
+        "185.199.111.153"
+      ]
+    }
+
+    apex_txt = {
+      type = "TXT"
+      name = "ruby.com.br"
+      value = "v=spf1 -all"
+    }
 
     # Diretório de comunidades Ruby
     # community.ruby.com.br -> https://github.com/guru-br/guru-br.github.io
     community = {
-        type  = "CNAME"
-        value = "guru-br.github.io"
+      type  = "CNAME"
+      name  = "community.ruby.com.br"
+      value = "guru-br.github.io"
     }
 
     # Diretório de empresas Ruby criado pela Le Wagon e Tropical.rb
     # empresas.ruby.com.br -> vast-guineafowl-iu7hx8tx58o0h63miig7mtb0.herokudns.com
     empresas = {
-        type = "CNAME"
-        value = "vast-guineafowl-iu7hx8tx58o0h63miig7mtb0.herokudns.com"
+      type  = "CNAME"
+      name  = "empresas.ruby.com.br"
+      value = "vast-guineafowl-iu7hx8tx58o0h63miig7mtb0.herokudns.com"
     }
 
     # Redirecionamento tropical.ruby.com.br
     # tropical.ruby.com.br -> www.tropicalrb.com (via https://github.com/wagner/tropical-ruby-com-br)
     tropical = {
-        type = "CNAME"
-        value = "wagner.github.io" # Repositório com redirecionamento manual
+      type  = "CNAME"
+      name  = "tropical.ruby.com.br"
+      value = "wagner.github.io" # Repositório com redirecionamento manual
     }
 
     # Arquitetura e Design de Aplicações Ruby (ada.ruby.com.br)
     # ada.ruby.com.br -> https://adarb.org
     ada = {
-        type = "CNAME"
-        value = "adarb.org"
+      type  = "CNAME"
+      name  = "ada.ruby.com.br"
+      value = "adarb.org"
     }
 
     # Frevo on Rails (frevo.ruby.com.br)
     # frevo.ruby.com.br -> https://github.com/frevo-on-rails/frevo-on-rails.github.com
     frevo = {
-        type = "CNAME"
-        value = "frevo-on-rails.github.io"
+      type  = "CNAME"
+      name  = "frevo.ruby.com.br"
+      value = "frevo-on-rails.github.io"
     }
 
     # Frevo on Rails (pe.ruby.com.br)
     # pe.ruby.com.br -> https://github.com/frevo-on-rails/frevo-on-rails.github.com
     pe = {
-        type = "CNAME"
-        value = "frevo-on-rails.github.io"
+      type  = "CNAME"
+      name  = "pe.ruby.com.br"
+      value = "frevo-on-rails.github.io"
     }
-    
   }
 }
